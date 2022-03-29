3a907d3ccecef69675cfbee7a18f73547926d6d9
root <slashmod6@gmail.com>
2022-03-29 22:55:01 +0000

    begin corejs

--- js/core.js
+++ js/core.js
@@ -1,1 +1,18 @@
-//hw
+function handleExecption(e){
+	console.error(e);
+	//more tba
+};
+(async function(){
+	try{
+		var k = await fetch("/msgv2/data/template.json");
+		k=await k.json();
+		var h = document.createElement("header");
+		h.innerHTML = k["header"];
+		document.body.prepend(h);
+		var f = document.createElement("footer");
+		f.innerHTML = k["footer"];
+		document.body.appendChild(f);
+	}catch(e){
+		handleExecption(e);
+	}
+})();
