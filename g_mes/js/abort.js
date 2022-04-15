dabab2117f36f166def7d023f1151e655eb43511
Logan L <thecrazykid2121@gmail.com>
2022-04-15 12:40:25 -0400

    settings-beta
    Just a commit to save/push settings  changes/additions

---
+++ g_mes/js/abort.js
@@ -0,0 +1,37 @@
+function getValue() {
+    var isChecked = document.getElementById("togAbort").checked;
+     
+    if(isChecked){
+        window.onblur = function () { 
+				
+
+            window.location.replace('https://classroom.google.com/h');
+             document.title = 'Classes';
+         }
+    } else {
+        window.onblur = function () { 
+             document.title = 'Classes';
+         }
+    }
+    var x = document.getElementById("abort1");
+   
+    if (x.style.display === "block") {
+      x.style.display = "block";
+   
+    } else {
+      x.style.display = "block";
+    }
+ }
+
+ function getSettings() {
+   
+    var x = document.getElementById("settings");
+   
+    if (x.style.display === "block") {
+      x.style.display = "none";
+   
+    } else {
+      x.style.display = "block";
+    }
+
+ }
