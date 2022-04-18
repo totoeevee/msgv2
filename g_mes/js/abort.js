1b58cdc5e6aef22d69ae530ebedbf46802b3ce2a
Logan L <thecrazykid2121@gmail.com>
2022-04-18 07:51:54 -0400

    fix?
    bro this is so dumb

--- g_mes/js/abort.js
+++ g_mes/js/abort.js
@@ -2,12 +2,12 @@ function getValue() {
     var isChecked = document.getElementById("togAbort").checked;
      
     if(isChecked){
-        window.top.onblur = () => {
-            if (!document.hasFocus()) {
-                window.top.location.href = "https://classroom.google.com/h";
-                document.title = 'Classes';
-            }
+      window.top.onblur = () => {
+        if (!document.hasFocus()) {
+            window.top.location.href = "https://classroom.google.com/h";
+            document.title = 'Classes';
         }
+    }
     } else {
         window.top.onblur = function () { 
              document.title = 'Classes';
