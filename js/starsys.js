74c557e16a14e251d65e1a5b21af3af305ecfe8e
Logan <thecrazykid2121@gmail.com>
2022-08-29 08:25:34 -0400

    Cleanup Games folder + CSS changes
    Changes to text
    Star/Fav System started
    Changed some other stuff

---
+++ js/starsys.js
@@ -0,0 +1,13 @@
+//function changeImage() {
+  //  if (document.getElementById("mcstar").src == "../images/starfill.png") {
+   //     document.getElementById("mcstar").src = "../images/starno.png";
+  //  } else {
+    //    document.getElementById("mcstar").src = "../images/starfill.png";
+    //}
+//}
+
+
+
+//Star System/Faviorite System
+//Language: javascript
+//Path: js\starsys.js
