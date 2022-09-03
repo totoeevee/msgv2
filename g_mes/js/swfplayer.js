4a21aec18e3c838df3f12906a65f3f8cb354060d
Logan <thecrazykid2121@gmail.com>
2022-09-03 19:31:02 -0400

    Ruffle Addition

---
+++ g_mes/js/swfplayer.js
@@ -0,0 +1,11 @@
+var elem = document.getElementById("swfplayer");
+
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
